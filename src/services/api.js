6f9c23568031f19a371fc2057746f86e446b685f
sanket chalwadi <sanket.s.chalwadi@gmail.com>
2026-01-19 09:03:17 +0530

    adding frontend on git

--- src/services/api.js
+++ src/services/api.js
@@ -2,5 +2,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://pokedex-backend-git-main-sanketschalwadi-7970s-projects.vercel.app/api/",
+  baseURL: "https://pokedex-backend-ffhl.onrender.com/api/pokemon",
 });
