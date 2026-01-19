980aac0adf158fdd3f5021c8e1f17642f9f9a6a6
sanket chalwadi <sanket.s.chalwadi@gmail.com>
2026-01-19 11:11:29 +0530

    adding frontend on git

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://pokedex-backend-eu2g.onrender.com//api"
+  baseURL: "https://pokedex-backend-eu2g.onrender.com/api"
 });
